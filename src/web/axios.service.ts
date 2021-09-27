4ce88f0ab19692db4cb39a03b8b1757a1f372d57
Hazork <arthur.fiorette@gmail.com>
2021-09-27 10:03:28 -0300

    feat: axios service

--- src/web/axios.service.ts
+++ src/web/axios.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import {
+  AxiosCacheInstance,
+  CacheAxiosResponse,
+  CacheRequestConfig,
+  createCache
+} from 'axios-cache-interceptor';
+import { MemoryStorage } from 'axios-cache-interceptor/dist/storage/memory';
 
 @Injectable()
-export class AxiosService {}
+export class AxiosService {
+  readonly axios: AxiosCacheInstance;
+
+  constructor() {
+    this.axios = createCache({
+      cache: {
+        ttl: 1000 * 30, // 30 Seconds
+        storage: new MemoryStorage(),
+        interpretHeader: true
+      }
+    });
+  }
+
+  request = async <T = any, R = CacheAxiosResponse<T>>(
+    config: CacheRequestConfig
+  ): Promise<R> => {
+    return this.axios.request(config);
+  };
+}
